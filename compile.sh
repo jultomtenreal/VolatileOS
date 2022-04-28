8041546772367b8969f798480affa0837a19081e
jultomtenreal <jultomtenriktig@gmail.com>
2022-04-28 19:23:52 +0200

    Can read a disk without data

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
-nasm bootloader.asm -f bin -o flp/bootloader.flp
+nasm "Sector 1/bootloader.asm" -f bin -o flp/bootloader.bin
+
+nasm "Sector 2+/ExtendedProgram.asm" -f bin -o flp/ExtendedProgram.bin
+
+cd flp
+cat bootloader.bin ExtendedProgram.bin > bootloader.flp
