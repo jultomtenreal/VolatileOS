caf2a9ae780b10ee65ec6bb0df217a1f134b7a8f
jultomtenreal <jultomtenriktig@gmail.com>
2022-04-29 07:06:52 +0200

    Removed compile.sh and fixed compile.bat

--- Compile scripts/Windows/compile.bat
+++ Compile scripts/Windows/compile.bat
@@ -1,3 +1,8 @@
 cd ..
 cd ..
-bash compile.sh
+nasm -f bin "Sector 1\bootloader.asm" -o flp\bin\bootloader.bin
+nasm -f bin "Sector 2+\ExtendedProgram.asm" -o flp\bin\ExtendedProgram.bin
+cd flp
+copy /b bin\bootloader.bin+bin\ExtendedProgram.bin bootloader.flp
+qemu-system-x86_64 bootloader.flp
+pause
--- compile.sh
+++
@@ -1,4 +0,0 @@
-nasm "Sector 1/bootloader.asm" -f bin -o flp/bin/bootloader.bin
-nasm "Sector 2+/ExtendedProgram.asm" -f bin -o flp/bin/ExtendedProgram.bin
-cd flp
-cat bin/bootloader.bin bin/ExtendedProgram.bin > bootloader.flp
